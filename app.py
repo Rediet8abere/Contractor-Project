31c90e16d6b646cd7b9822985180ac6f6a8d1b2f
Rediet8abere <redietaberegesesse@gmail.com>
2019-10-09 12:30:12 -0700

    login issue solved

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from forms import RegistrationForm, LoginForm
 import os
 
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/MoviesF')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 movies = db.movies
@@ -94,6 +94,7 @@ def login():
     """Takes user to regestration page"""
     form = LoginForm()
     if request.method == 'POST':
+        print("Hey in request method")
         if form.validate_on_submit():
             if users.find_one({"email": form.email.data}):
                 if (form.email.data == users.find_one({"email": form.email.data})["email"]) and (form.password.data == users.find_one({"email": form.email.data})["password"]):
@@ -116,4 +117,4 @@ def playlists_edit(movie_id):
     return render_template('movies_edit.html', movie=movie)
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))    
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
